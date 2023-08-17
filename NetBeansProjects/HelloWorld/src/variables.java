7ca0cb9e41c43a85e605c2309efc8a9e9efc3ad2
irma660 <1325455@senati.pe>
2023-08-17 10:52:10 -0500

    eligiendo opción

--- NetBeansProjects/HelloWorld/src/variables.java
+++ NetBeansProjects/HelloWorld/src/variables.java
@@ -19,6 +19,10 @@ public class variables {
         }else if (opcion <2){
             System.out.println("ESA OPCIÓN NO ESTA DISPONIBLE");   
         }
+        System.out.println("INGRESA EL PRIMER NÚMERO");
+        x = sc.nextInt();
+        System.out.println("INGRESA EL SEGUNDO NÚMERO");
+        y = sc.nextInt();
         
               
      
