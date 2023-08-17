a526ee6b8c164a6bb4abdeb60861c78de9d42a8a
irma660 <1325455@senati.pe>
2023-08-17 10:45:46 -0500

    creando menú

--- NetBeansProjects/HelloWorld/src/variables.java
+++ NetBeansProjects/HelloWorld/src/variables.java
@@ -8,10 +8,17 @@ public class variables {
         double resultado;
         double x;
         double y;
+        System.out.println("-----BIENVENID@ A TU CALCULADORA-----");
+        System.out.println("==========ELIGE UNA OPCIÓN===========");
+        System.out.println("1. SUMAR");
+        System.out.println("2. RESTAR");
+        opcion = sc.nextInt();
         
+        if (opcion == 1){
+            System.out.println("ELIGIÓ LA OPCIÓN DE SUMAR");
+        }
         
-
-  
+         
      
     }
     
