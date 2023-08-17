f643e7a3263e0e25c645e99d437ba1f57950686b
irma660 <1325455@senati.pe>
2023-08-17 10:37:11 -0500

    hello world

--- NetBeansProjects/HelloWorld/src/helloworld/HelloWorld.java
+++ NetBeansProjects/HelloWorld/src/helloworld/HelloWorld.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world, soy Irma");
     }
     
 }
--- NetBeansProjects/HelloWorld/src/variables.java
+++ NetBeansProjects/HelloWorld/src/variables.java
@@ -1,19 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author Alumno
- */
+import java.util.Scanner; 
 public class variables {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner (System.in);
+        int opcion;
+        double resultado;
+        double x;
+        double y;
+        
+        
+
+        
+         
+     
     }
     
 }
