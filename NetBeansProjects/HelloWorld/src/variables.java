05b43d70aa74ba5e4629cbc51a9d1908a7d3ba8f
irma660 <1325455@senati.pe>
2023-08-17 11:31:24 -0500

    corregí

--- NetBeansProjects/HelloWorld/src/variables.java
+++ NetBeansProjects/HelloWorld/src/variables.java
@@ -1,17 +1,18 @@
 import java.util.Scanner; 
 public class variables {
 
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int opcion;
         double resultado;
         double x;
         double y;
-     
         System.out.println("-----BIENVENID@ A TU CALCULADORA-----");
         System.out.println("==========ELIGE UNA OPCIÓN===========");
         System.out.println("1. SUMAR");
         System.out.println("2. RESTAR");
+        System.out.println("3. SALIR");
         opcion = sc.nextInt();
         
         
@@ -31,7 +32,10 @@ public class variables {
                 break;
             default:
                 System.out.println("NÚMERO INVÁLIDO");            
-        
         }
-    } 
+        
+              
+     
+    }
+    
 }
